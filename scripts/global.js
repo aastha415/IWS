15b7e95cf46e331dba5edad9f8d82d92af68f83d
Aastha Kedia <31680972+aastha415@users.noreply.github.com>
2019-11-26 14:19:57 +0530

    Update global.js

--- scripts/global.js
+++ scripts/global.js
@@ -1,8 +1,8 @@
 var colleges = [
-  new College("New York University", 2.4, 310, ["Biology", "Theatre"], "Community", 3000),
-  new College("Harvard Uiversity", 3.0, 290, ["Chemistry", "Law"], "Public", 20000),
-  new College("University of California - Berkeley", 3.8, 323, ["Mathematics", "Computer Science"], "Private", 30000),
-  new College("Stanford Univeristy", 3.6, 315, ["Psychology", "Philosophy"], "Private", 32500)
+  new College("New York University", 2.4, 310, ["Computer Science"], "Community", 3000),
+  new College("Harvard Uiversity", 3.0, 290, ["Computer Science"], "Public", 20000),
+  new College("University of California - Berkeley", 3.8, 323, ["Computer Science"], "Private", 30000),
+  new College("Stanford Univeristy", 3.6, 315, ["Computer Science"], "Private", 32500)
   // new College("Indian School Business", 3.6, 320, ["Management"], "Private", 325000)
 
 ];
